813063f8a72aad775b6d763f2fbf8e9ff17af3ce
diegooo555 <dieguito555alf@outlook.es>
2025-05-08 09:54:31 -0500

    Config vite updated

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist'
-  },
-  server: {
-    historyApiFallback: true
-  },
-  preview: {
-    // 🔁 Redirige todas las rutas al index.html
-    // para que funcione el BrowserRouter
-    fallback: '/index.html'
   }
 })
